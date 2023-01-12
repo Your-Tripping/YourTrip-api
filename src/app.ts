f8096e93af1426c680bb6f2a94fd4939bccff454
Filipe Monteiro Fernandes <filipemonteirofernandes9@gmail.com>
2023-01-12 15:22:37 -0300

    feat: add login route

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import "express-async-errors";
 import express from "express";
 import { errorHandler } from "./error/errors";
 import userRoutes from "./routes/user.routes";
+import sessionRoutes from "./routes/session.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("", sessionRoutes)
 
 app.use(errorHandler);
 
