6f8c7f0a47530035f4bccaaed8b8c837044b5890
unknown <mickelbarros@yahoo.com>
2023-01-13 17:52:56 -0300

    feat: add comments, likes & follow tests

--- src/__tests__/integration/postsTests/postsRoutes.test.ts
+++ src/__tests__/integration/postsTests/postsRoutes.test.ts
@@ -54,6 +54,18 @@ describe("/posts", () => {
      
     })
 
+    test("GET /posts/:id -  Must be able to list users'post", async () => {
+        await request(app).post('/users').send(mockedAdmin)
+
+        const adminLoginResponse = await request(app).post("/login").send(mockedAdminLogin);
+        const UserToBeListed= await request(app).get('/users').set("Authorization", `Bearer ${adminLoginResponse.body.token}`)
+    
+        const response = await request(app).get(`/posts/${UserToBeListed.body[1].id}`)
+        expect(response.body).toHaveLength(1)
+        expect(response.status).toBe(200)
+     
+    })
+
     test("UPDATE /posts - Must be able to update posts", async () => {
         const posts = await request(app).get('/posts')
         const adminLoginResponse = await request(app).post("/login").send(mockedAdminLogin)
