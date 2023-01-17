1979f28f4aa4ad5ce2ae650a4bf2daf324d0c554
JunielsonDiniz <juniosantos_@hotmail.com>
2023-01-17 10:49:13 -0300

    feat: add verifyComment.middleware. modified comments.routes

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -3,11 +3,17 @@ import createCommentsController from "../controllers/comments/createComments.con
 import deleteCommentsController from "../controllers/comments/deleteComments.controller";
 import updateCommentsController from "../controllers/comments/updateComments.controller";
 import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
+import verifyCommentMiddleware from "../middlewares/verifyComment.middleware";
 
 const commentsRoutes = Router();
 
-commentsRoutes.post("/:id", tokenAuthMiddleware, createCommentsController);
-commentsRoutes.patch("/:id", tokenAuthMiddleware, updateCommentsController);
+commentsRoutes.post("/post/:id", tokenAuthMiddleware, createCommentsController);
+commentsRoutes.patch(
+  "/:id",
+  tokenAuthMiddleware,
+  verifyCommentMiddleware,
+  updateCommentsController
+);
 commentsRoutes.delete("/:id", tokenAuthMiddleware, deleteCommentsController);
 
 export default commentsRoutes;
